f19e841023fb724618dc9d0a9b03e611e45b1abd
rutvik11062000 <rutvik.chandla@gmail.com>
2020-05-09 15:15:34 +0530

    Updates

---
+++ precache-manifest.11aec69eee4088b1cb82a3a105e62258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47c72d8a1b74e7dc5f135b8b9c006b6",
+    "url": "/Monster/index.html"
+  },
+  {
+    "revision": "1a766d454682b2eeef8b",
+    "url": "/Monster/static/css/main.23eee242.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/Monster/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a766d454682b2eeef8b",
+    "url": "/Monster/static/js/main.624a6baa.chunk.js"
+  },
+  {
+    "revision": "f2d3881b3e4a10bcecee",
+    "url": "/Monster/static/js/runtime-main.bb5c1d13.js"
+  }
+]);
